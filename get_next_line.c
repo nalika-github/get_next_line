b85bbaf5de3ccd58f7fab92e892c11b8733c6c76
nalika-github <g.nalika.tiew@gmail.com>
2022-05-27 12:19:54 +0700

    5th

--- get_next_line.c
+++ get_next_line.c
@@ -19,7 +19,7 @@ static char	*read_file(int fd, char *stash)
 
 	buf = ft_calloc(BUFFER_SIZE + 1, 1);
 	n_byte = 1;
-	while (n_byte && !ft_isn_line(buf) && !ft_isn_line(stash))
+	while (n_byte && !ft_isn_line(stash))
 	{
 		n_byte = read(fd, buf, BUFFER_SIZE);
 		if (n_byte == -1)
@@ -34,13 +34,11 @@ static char	*read_file(int fd, char *stash)
 	return (stash);
 }
 
-static char	*get_line(char *stash)
+static char	*get_line(char *stash, size_t line_len)
 {
-	size_t	line_len;
 	size_t	i;
 	char	*line;
 
-	line_len = ft_line_len(stash);
 	if (line_len == 0)
 		return (0);
 	line = ft_calloc(line_len + 1, 1);
@@ -55,14 +53,14 @@ static char	*get_line(char *stash)
 	return (line);
 }
 
-static char	*update_stash(char *stash, char *line)
+static char	*update_stash(char *stash, size_t line_len)
 {
 	size_t	i;
 	size_t	j;
 	size_t	stash_len;
 	char	*new_stash;
 
-	i = ft_strlen(line);
+	i = line_len;
 	stash_len = ft_strlen(stash);
 	new_stash = ft_calloc(stash_len - i + 1, 1);
 	j = 0;
@@ -80,15 +78,17 @@ char	*get_next_line(int fd)
 {
 	static char	*stash;
 	char		*line;
+	size_t		line_len;
 
 	if (!stash)
 		stash = ft_calloc(1, 1);
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
 	stash = read_file(fd, stash);
-	line = get_line(stash);
+	line_len = ft_line_len(stash);
+	line = get_line(stash, line_len);
 	if (!line)
 		return (0);
-	stash = update_stash(stash, line);
+	stash = update_stash(stash, line_len);
 	return (line);
 }
